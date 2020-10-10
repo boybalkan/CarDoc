fa7dd375b6879991d37aa450cf5280a21849d71c
boybalkan <boybalkan97@gmail.com>
2020-10-10 15:14:20 +0200

    addded more cars in json

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -34,8 +34,8 @@ constructor(private http: HttpClient, private dataService :DataService){
       for(let j = 0; j < this.information[i].children.length; j++){
         for(let k = 0; k < this.information[i].children[j].children.length; k++){
         this.items.push({
-        marke: this.information[i].name ,
-        modell: this.information[i].children[j].name,
+        marke: this.information[i].brand ,
+        modell: this.information[i].children[j].modell,
         hsnTsn: this.information[i].children[j].children[k].hsnTsn 
       });
     }  
@@ -77,84 +77,18 @@ sucheAutoHersteller(ev: CustomEvent){
   this.information = this.allInformation;
   const val = ev.detail.value;
   
-  console.log("All: " + this.information[1].children[1].children[1].hsnTsn);  
+  //console.log("All: " + this.information[1].children[1].children[1].hsnTsn);  
   //console.log(val);
   if(val && val.trim() !== ''){
     
       this.information = this.information.filter(data => {
-        console.log(data.children);
-        return data.name.toLocaleLowerCase().indexOf(val.trim().toLocaleLowerCase()) > -1 
-               
-         ;
-         
+       // console.log(data.children);
+        return data.brand.toLocaleLowerCase().indexOf(val.trim().toLocaleLowerCase()) > -1;  
       });
-      for(let i=0; i < this.allInformation.length; i++){
-    }
    
   }
   
 }
 
- 
 
-
-
-
-
-
-/*
-  private hersteller = ['Audi', 'BMW', 'Mercedes-Benz', 'Volkswagen', 'Ford', '',
-     'Abarth','Alfa Romeo', 'Alpina', 'Alpine', 'Aston Martin','Bentley', 'Brabus', 
-     'Bugatti', 'Cadillac', 'Chevrolet', 'Chrysler', 'Citroen', 'Corvette', 'Dacia', 
-     'Daihatsu', 'Dodge' ];
-
-  private modelleAudi = ['Audi A1', 'Audi A2', 'Audi A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
-
-  public items: Array<{ title:string; icon:string}> = [];
-  public allItems: Array<{ title: string; icon: string }> = [];
-
-  public selected: String = '';
-
-
-  constructor(private router: Router) {
-    for(let i = 0; i < this.hersteller.length; i++){
-      this.items.push({
-        title: this.hersteller[i].charAt(0).toUpperCase() + this.hersteller[i].slice(1),
-        icon: this.hersteller[i]
-      });
-    }
-    this.allItems = this.items;
-  }
-
-  ngOnInit(){
-    }
-
-  sucheAutoHersteller(ev: CustomEvent){
-    this.items = this.allItems;
-    const val = ev.detail.value;
-
-    if(val && val.trim() !== ''){
-      this.items = this.items.filter(term => {
-        return term.title.toLocaleLowerCase().indexOf(val.trim().toLocaleLowerCase()) > -1;
-      });
-    }
-  }
-
- 
-
-  zeigeAudiModellAn(event: any){
-    
-    const value = event.target.value;
-    this.selected = value;
-    console.log(event);
-    console.log(this.selected);
-
-    if(value == "a1"){
-      console.log("Das ist ein Audi A1");
-    }
-  }
- 
- 
-  
-  */
 }
